3374d2ab8fdc17b0417054f5fa13aef0f267b417
dan norwood <norwood@confluent.io>
2017-06-08 09:57:42 -0700

    initial

---
+++ src/io/confluent/jenkins/Common.groovy
@@ -0,0 +1,16 @@
+def notifySlack() {
+  switch (currentBuild.currentResult) {
+    case 'SUCCESS':
+      if (currentBuild.previousBuild != null && currentBuild.previousBuild.currentResult != 'SUCCESS') {
+        slackSend(channel: '#clients-eng', color: 'good', message: "${env.JOB_NAME} - #[${env.BUILD_NUMBER}] Success <${env.BUILD_URL}|(Open)>", teamDomain: 'confluent')
+      }
+      break;
+    case 'UNSTABLE':
+      slackSend(channel: '#clients-eng', color: 'YELLOW', message: "${env.JOB_NAME} - #[${env.BUILD_NUMBER}] Unstable <${env.BUILD_URL}|(Open)>", teamDomain: 'confluent')
+      break;
+    case 'FAILURE':
+    default:
+      slackSend(channel: '#clients-eng', color: 'bad', message: "${env.JOB_NAME} - #[${env.BUILD_NUMBER}] Failure <${env.BUILD_URL}|(Open)>", teamDomain: 'confluent')
+      break;
+  }
+}
